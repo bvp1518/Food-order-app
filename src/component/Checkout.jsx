304f4007d76bdefadf998ade41b9d4cf1ce7de70
bvp1518 <bvp18061992@gmail.com>
2024-08-20 12:13:33 +0530

    till-277. Handling Form Submission & Validation 6min Play 278. Sending a POST Request with Order Data

--- src/component/Checkout.jsx
+++ src/component/Checkout.jsx
@@ -16,30 +16,55 @@ export default function Checkout() {
         (totalPrice, item) => totalPrice + item.quantity * item.price,
         0
     );
-function handleClose(){
-    userProgressCtx.hideCheckout();
+    function handleClose() {
+        userProgressCtx.hideCheckout();
+    }
+
+function handleSubmit(event){
+  event.preventDefault();
+
+  const fd = new FormData(event.target);
+  const customerData = object.fromEntries(fd.entries());
+
+  fetch('http://localhost:3000/orders',{
+      method:'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+         order: {
+            items: cartCtx.items,
+            customer:customerData
+         }
+      })
+  });
+
 }
-  
+
+
+
 
     return (
-   <Modal open={userProgressCtx.progress === 'checkout'} onClose={handleClose} >
-    <form>
-        <h2>Checkout</h2>
-        <p>Total Amount:{currencyFormatter.format(cartTotal)}</p>
-    
-    <Input label="Full Name" type="text"  id="full-name" />
-    <Input label="E-mail Address" id="email" type="email" />
-    <Input label="Street" type="text" id="street" />
-    <div className='control-row'>
-        <Input label="Postal code" type="text" id="postal-code" />
-        <Input label="city" type="text" id="city" />
-    </div>
+        <Modal open={userProgressCtx.progress === 'checkout'} onClose={handleClose} >
+            <form onSubmit={handleSubmit}>
+                <h2>Checkout</h2>
+                <p>Total Amount:{currencyFormatter.format(cartTotal)}</p>
 
-    <p className='modal-actions'>
-        <Button type="button" textOnly onClick={handleClose}>Close</Button>
-        <Button>Submit Order</Button>
-    </p>
-    </form>
-   </Modal>    
-);
+                <Input label="Full Name" type="text" id="name" />
+                <Input label="E-mail Address" id="email" type="email" />
+                <Input label="Street" type="text" id="street" />
+                <div className='control-row'>
+                    <Input label="Postal code" type="text" id="postal-code" />
+                    <Input label="city" type="text" id="city" />
+                </div>
+
+                <p className='modal-actions'>
+                    <Button type="button" textOnly onClick={handleClose}>
+                        Close
+                    </Button>
+                    <Button>Submit Order</Button>
+                </p>
+            </form>
+        </Modal>
+    );
 }
